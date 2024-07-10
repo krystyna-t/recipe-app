3c42dcbc8069f3f28cd59cd8a449beaeac9e4222
krystyna <krystyna@indeed.com>
2024-07-10 23:34:22 +0900

    Updated styling for table

--- src/RecipeView.js
+++ src/RecipeView.js
@@ -6,7 +6,7 @@ function RecipeView({ recipe, deleteRecipe }) {
       <tr>
         <td>{recipe.name}</td>
         <td>{recipe.cuisine}</td>
-        <td><img src={recipe.photo}/></td>
+        <td><img src={recipe.photo} alt={recipe.name}/></td>
         <td className="content_td"><p>{(recipe.ingredients)}</p></td>
         <td className="content_td"><p>{(recipe.preparation)}</p></td>
         <td>
