f324bfdefebe1516cd55959269a5b261cff2d48e
krystyna <krystyna@indeed.com>
2024-07-10 22:13:58 +0900

    Updates

---
+++ precache-manifest.44bc026d6c750cae7c615bf7301d583a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92973108780573f04dbdcb4b07dff280",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "2240fdbfe7cb11c8bd82",
+    "url": "/recipe-app/static/css/main.da0f0e26.chunk.css"
+  },
+  {
+    "revision": "d5b4d0fb8be146f2f199",
+    "url": "/recipe-app/static/js/2.07acb89f.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/recipe-app/static/js/2.07acb89f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2240fdbfe7cb11c8bd82",
+    "url": "/recipe-app/static/js/main.0eb49688.chunk.js"
+  },
+  {
+    "revision": "9ab47c757d89f93c0d01",
+    "url": "/recipe-app/static/js/runtime-main.804532aa.js"
+  }
+]);
